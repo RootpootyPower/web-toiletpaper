d98b5d5f80db71f07076345ede03e9774030edee
weaselovercome <averymccrae1@hotmail.com>
2024-04-18 19:37:23 -0400

    comic controls fix
    wasnt broken per se, but now it's added in with javascript and doesn't break if you put in an invalid number. also adjusted comic list layout

--- comics/gallery.js
+++ comics/gallery.js
@@ -1,22 +1,41 @@
 var gallery = document.getElementById("comics");
-var controls = document.getElementById("controls");
-var controls2 = controls.cloneNode(true);
-controls2.id = "controls2";
-document.getElementById("maincontent").appendChild(controls2);
-var input = document.querySelector("#controls #numinput");
-var input2 = document.querySelector("#controls2 #numinput");
+var length = gallery.children.length;
 
-changeImage(1); // initialize it on first page
+// only actually add controls if there's more than one page
+if (length > 1) {
+    var controls = document.createElement("div");
+    controls.id = "controls";
+    controls.className = "controls";
+    controls.innerHTML =
+    `<button onclick="first();">«</button>
+    <button onclick="back();">back</button>
+    <input id="numinput" type="number" min="1" max="${length}" value="1">
+    <button onclick="forward();">next</button>
+    <button onclick="last();">»</button>
+    `;
+    gallery.parentElement.insertBefore(controls, gallery);
+    var input = document.querySelector("#controls #numinput");
+
+    var controls2 = controls.cloneNode(true);
+    controls2.id = "controls2";
+    document.getElementById("maincontent").appendChild(controls2);
+    var input2 = document.querySelector("#controls2 #numinput");
+
+    changeImage(1); // initialize it on first page
+}
 
 // change image on enter
 input.addEventListener("keyup", (e) => {
     if (e.key == "Enter") {
-        changeImage(input.value);
+        // avoid completely hiding it if the user is stupid
+        if (input.value > 0 && input.value <= length)
+            changeImage(input.value);
     }
 });
 input2.addEventListener("keyup", (e) => {
     if (e.key == "Enter") {
-        changeImage(input2.value);
+        if (input.value > 0 && input.value <= length)
+            changeImage(input2.value);
     }
 });
 
@@ -37,10 +56,7 @@ function changeImage(image) {
     input.value = current;
     input2.value = current;
 
-    // go through all but selected and hide them
-    let len = gallery.children.length;
-
-    for (let i = 0; i < len; i++) {
+    for (let i = 0; i < length; i++) {
         // MAKE IT ZERO INDEXED
         let show = i == image-1 ? "block" : "none";
         gallery.children.item(i).style.display = show;
@@ -55,7 +71,7 @@ function back() {
 }
 
 function forward() {
-    if (current+1 > input.max) {
+    if (current+1 > length) {
         return;
     }
     changeImage(current+1);
@@ -66,5 +82,5 @@ function first() {
 }
 
 function last() {
-    changeImage(input.max);
+    changeImage(length);
 }
